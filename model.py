cdbb110ac27ed14b3f9f9bab77797a91c0245edd
Yodaman07 <ayaan.irshad@gmail.com>
2024-05-17 10:51:14 -0700

    Some more testing was done, now for adaptive improvements

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ class Model:
         group = []
         for i in range(start, count + start):
             img = Image.open(
-                f"custom_training/num{i}.png").convert(
+                f"custom_predictions/num{i}.png").convert(
                 "L")  # https://stackoverflow.com/questions/48121916/numpy-resize-rescale-image resizing help
             img = PIL.ImageOps.invert(img)  # implement auto invert
             numpyImg = numpy.array(
@@ -53,16 +53,16 @@ class Model:
             if predictions is None:
                 plt.xlabel(label_list[i])
             else:
-                print(i)
                 prediction = predictions[i]
+                # print(i)
+                # print(prediction)
                 num = int(np.argmax(prediction))
                 percentage = np.max(prediction) * 100
                 color = 'red'
                 if num == int(label_list[i]):
                     color = 'blue'
                 else:
-                    pass
-                    # print(num, realLabels[i])
+                    print(num, label_list[i])
                 plt.xlabel(f"{self.classNames[num]} ; {np.round(percentage, 2)}").set_color(color)
 
         plt.show()
@@ -81,7 +81,7 @@ class HandWrittenNumbersModel(Model):
             keras.layers.Dense(258, activation="relu"),
             keras.layers.Dense(10)
         ])  # setting up the model
-        model.compile(optimizer="adam", loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        model.compile(optimizer="Adam", loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                       metrics=['accuracy'])  # compiling everything, now ready to train the model
         model.fit(self.train_img, self.train_label)  # training the model
         self.model = model
@@ -90,8 +90,9 @@ class HandWrittenNumbersModel(Model):
         #  If CUSTOM_IMAGE is left blank, program will use the default testing images
         prediction_model = keras.Sequential([self.model, keras.layers.Softmax()])
         if CUSTOM_IMAGE is not None:
+            labels = numpy.array([1, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 7]) # num0 to num11
             predictions = prediction_model.predict(CUSTOM_IMAGE)
-            self.displayData(self, 11, CUSTOM_IMAGE, ["1", "2", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+            self.displayData(self, 2, CUSTOM_IMAGE, [3, 5],
                              predictions)
         else:
             predictions = prediction_model.predict(self.test_img)
@@ -102,8 +103,8 @@ m = HandWrittenNumbersModel()
 # model = Model(keras.datasets.mnist.load_data(path="mnist.npz"))
 # model.evalCustomImages(2)
 # model.displayData(2, model.evalCustomImages(2), ["1", "2"], False)
-
 input("Ready? ")
 m.trainModel()
 input("Trained! Ready for the predictions? ")
-m.predict(CUSTOM_IMAGE=m.evalCustomImages(11))
+m.predict(m.evalCustomImages(2, 12))
+
